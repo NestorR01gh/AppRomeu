b78324988801745a5ceb88651acfa3fdd2aa5357
NestorR01 <neroaz@floridauniversitaria.es>
2022-04-16 23:48:34 +0200

    Implementando traducción en employee screen

--- src/screens/EmployeeScreen.js
+++ src/screens/EmployeeScreen.js
@@ -4,8 +4,9 @@ import { DisplayDataBox } from '../components/DisplayDataBox';
 import { LoadingModal } from '../components/LoadingModal';
 import { backgroundColor, fontFamily, urlApi } from '../utils/Constants';
 import { Request } from '../utils/Request';
+import { withTranslation } from 'react-i18next';
 
-export class EmployeeScreen extends Component {
+class EmployeeScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -41,6 +42,7 @@ export class EmployeeScreen extends Component {
     }
 
     render() {
+        const {t} = this.props;
         return (
             <View style={styles.container}>
                 <LoadingModal color={backgroundColor} animating={this.state.loading} />
@@ -55,29 +57,29 @@ export class EmployeeScreen extends Component {
                                 <Text style={styles.title}>{this.state.data.iusu}</Text>
                             </View>
                             <View style={styles.labelsImageView}>
-                                <DisplayDataBox editable={false} label="Posición" icon="account-cog" value={this.state.data.title} />
-                                <DisplayDataBox editable={false} label="Compañía" icon="briefcase-variant-outline" value={this.state.data.company} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.position")} icon="account-cog" value={this.state.data.title} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.company")} icon="briefcase-variant-outline" value={this.state.data.company} />
                             </View>
                         </View>
                         <View style={styles.body}>
-                            <DisplayDataBox editable={false} label="Correo" icon="email" value={this.state.data.emailAddress} />
-                            <DisplayDataBox editable={false} label="Dirección" icon="map-marker" value={this.state.streetAddress} />
-                            <DisplayDataBox editable={false} label="Mánager" icon="account-tie" value={this.state.data.manager} />
+                            <DisplayDataBox editable={false} label={t("employeeScreen.mail")} icon="email" value={this.state.data.emailAddress} />
+                            <DisplayDataBox editable={false} label={t("employeeScreen.adress")} icon="map-marker" value={this.state.streetAddress} />
+                            <DisplayDataBox editable={false} label={t("employeeScreen.manager")} icon="account-tie" value={this.state.data.manager} />
                             <View style={styles.doubleInputView}>
-                                <DisplayDataBox editable={false} label="Oficina" icon="office-building-marker" value={this.state.data.office} />
-                                <DisplayDataBox editable={false} label="Nombre inicio sesión" icon="account-lock-open" value={this.props.route.params.loginName} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.office")} icon="office-building-marker" value={this.state.data.office} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.loginName")} icon="account-lock-open" value={this.props.route.params.loginName} />
                             </View>
                             <View style={styles.doubleInputView}>
-                                <DisplayDataBox flex={0.5} editable={false} icon="numeric" label="Extensión" value={this.state.data.extension} />
-                                <DisplayDataBox editable={false} label="Teléfono" icon="phone" value={this.state.data.phone} />
+                                <DisplayDataBox flex={0.5} editable={false} icon="numeric" label={t("employeeScreen.extension")} value={this.state.data.extension} />
+                                <DisplayDataBox editable={false} label="Teléfono" icon={t("employeeScreen.phone")} value={this.state.data.phone} />
                             </View>
                             <View style={styles.doubleInputView}>
-                                <DisplayDataBox flex={0.5} editable={false} icon="numeric" label="Extensión" value={this.state.data.extensionMobile} />
-                                <DisplayDataBox editable={false} label="Teléfono móvil" icon="cellphone" value={this.state.data.mobile} />
+                                <DisplayDataBox flex={0.5} editable={false} icon="numeric" label={t("employeeScreen.extension")} value={this.state.data.extensionMobile} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.mobilePhone")} icon="cellphone" value={this.state.data.mobile} />
                             </View>
                             <View style={styles.doubleInputView}>
-                                <DisplayDataBox editable={false} label="País" icon="flag" value={this.state.data.country} />
-                                <DisplayDataBox editable={false} label="Departamento" icon="account-group" value={this.state.data.department} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.country")} icon="flag" value={this.state.data.country} />
+                                <DisplayDataBox editable={false} label={t("employeeScreen.department")} icon="account-group" value={this.state.data.department} />
                             </View>
                         </View>
                     </View>
@@ -128,3 +130,5 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     }
 });
+
+export default withTranslation("global")(EmployeeScreen)
